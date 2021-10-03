293639bbef23d3e3eedbeef9ca78fc0ead0e3acd
Livspace <livspace@Admins-MacBook-Pro.local>
2021-10-03 14:18:01 +0530

    test commit

---
+++ src/main/java/com/example/zolve/App.java
@@ -0,0 +1,13 @@
+package com.example.zolve;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
